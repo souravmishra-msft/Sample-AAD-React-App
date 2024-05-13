32d831c1fdfca68e8011fa9327f806beed856c42
Sourav Mishra <souravmishra-msft@outlook.com>
2024-05-13 21:30:25 +0530

    minor updates done

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-        clientId: 'db592d32-7af0-4a55-ac86-e5c04105df21',
+        clientId: '<client-id>',
         authority: 'https://login.microsoftonline.com/common',
         redirectUri: 'http://localhost:3000',
         postLogoutRedirectUri: 'http://localhost:3000',
